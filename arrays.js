ba95581d763daa8bc5aa2f4eb579cc603f6be211
Elise Bonner <elise0209@mac.com>
2017-10-29 02:17:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,7 +42,7 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
 
-function destructivelyAddElementToEndOfArray(array){
+function destructivelyRemoveElementFromEndOfArray (array){
   array.pop();
   return array;
 }
